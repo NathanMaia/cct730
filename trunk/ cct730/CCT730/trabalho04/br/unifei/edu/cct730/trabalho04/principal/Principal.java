d12018df11a59e651bdca55bc79a2b668d05ab62
fknappe <fknappe@33ec694b-4ff1-6dd7-de2a-6761cad78f61>
2011-09-11 21:13:39 +0000

    - melhoria na documentacao e comentarios

--- trunk/ cct730/CCT730/trabalho04/br/unifei/edu/cct730/trabalho04/principal/Principal.java
+++ trunk/ cct730/CCT730/trabalho04/br/unifei/edu/cct730/trabalho04/principal/Principal.java
@@ -3,6 +3,8 @@ package br.unifei.edu.cct730.trabalho04.principal;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.UIManager;
+
 import br.unifei.edu.cct730.trabalho04.principal.gui.JanelaPrincipal;
 
 /**
@@ -19,6 +21,28 @@ public class Principal {
 	 * @param String args
 	 */
 	public static void main(String args[]) {
+		/* 
+		 * Definindo todas as mensagens da aplicacao localizadas para pt-BR 
+		 */
+		UIManager.put("OptionPane.yesButtonText", "Sim");  
+        UIManager.put("OptionPane.cancelButtonText", "Cancelar");  
+        UIManager.put("OptionPane.noButtonText", "Não");  
+        UIManager.put("OptionPane.okButtonText", "OK");
+        
+        UIManager.put("FileChooser.cancelButtonText", "Cancelar");
+        UIManager.put("FileChooser.openButtonText", "Abrir");
+        UIManager.put("FileChooser.openButtonToolTipText", "Abrir arquivo selecionado");
+        UIManager.put("FileChooser.saveButtonText", "Salvar");
+        UIManager.put("FileChooser.saveButtonToolTipText", "Salvar arquivo selecionado");
+		UIManager.put("FileChooser.newFolderAccessibleNam", "Arquivo novo");
+		UIManager.put("FileChooser.fileNameLabelText", "Arquivo:");
+		UIManager.put("FileChooser.upFolderAccessibleName", "Acima");
+		UIManager.put("FileChooser.upFolderToolTipText", "Acima um nível"); 
+		UIManager.put("FileChooser.homeFolderAccessibleName", "Inicio");
+		UIManager.put("FileChooser.filesOfTypeLabelText", "Formato de arquivo:");
+		UIManager.put("FileChooser.acceptAllFileFilterText", "*.*");
+		UIManager.put("FileChooser.lookInLabelText", "Visualizar");
+		
 		final JanelaPrincipal janela = new JanelaPrincipal("Processamento de Imagens - Equalizacao de Imagens");
 		//Tratamento da ação de fechar a janela do aplicativo
 		janela.addWindowListener(new WindowAdapter() {
